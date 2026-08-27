7de9c32e0d2d482fb90373c9e9b67e1602a6a7c5
Zainul Abdeen <143203808+zainul991@users.noreply.github.com>
2026-08-27 13:31:04 +0530

    Update home.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -26,7 +26,7 @@ function HomeComponent() {
 
                 <div style={{ display: "flex", alignItems: "center" }}>
 
-                    <h2>Apna Video Call</h2>
+                    <h2>SyncMeet</h2>
                 </div>
 
                 <div style={{ display: "flex", alignItems: "center" }}>
