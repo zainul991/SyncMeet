dc580140f1c9730f17f742ed71c805966431f8c1
Zainul Abdeen <143203808+zainul991@users.noreply.github.com>
2026-08-27 13:29:44 +0530

    Update landing.jsx

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -10,7 +10,7 @@ export default function LandingPage() {
         <div className='landingPageContainer'>
             <nav>
                 <div className='navHeader'>
-                    <h2>Apna Video Call</h2>
+                    <h2>SyncMeet</h2>
                 </div>
                 <div className='navlist'>
                     <p onClick={() => {
@@ -34,7 +34,7 @@ export default function LandingPage() {
                 <div>
                     <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
 
-                    <p>Cover a distance by Apna Video Call</p>
+                    <p>Cover a distance by SyncMeet</p>
                     <div role='button'>
                         <Link to={"/auth"}>Get Started</Link>
                     </div>
